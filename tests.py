c6a3460e474d05375445dd495b8ad4b16e6cc7d4
Chris J. Karr <chris@audacious-software.com>
2026-05-26 19:17:47 +0000

    Attempting to use threads to simulate a race condition to test

--- tests.py
+++ tests.py
@@ -1,26 +1,52 @@
-import asyncio
+import time
+
+from threading import Thread
 
 from django.test import TestCase
 
 from .decorators import handle_named_lock
 
+RESULTS = {}
+
 @handle_named_lock(lock_name='sleep_func')
-async def sleep_func(sleep_for=10):
-    await asyncio.sleep(sleep_for)
+def sleep_func(sleep_for=10):
+    time.sleep(sleep_for)
 
     return True
+
+# https://medium.com/@birenmer/threading-the-needle-returning-values-from-python-threads-with-ease-ace21193c148
+
+class CustomThread(Thread):
+    def __init__(self, group=None, target=None, name=None, args=(), kwargs={}, verbose=None):
+        # Initializing the Thread class
+        super().__init__(group, target, name, args, kwargs)
+        self._return = None
+
+    # Overriding the Thread.run function
+    def run(self):
+        if self._target is not None:
+            self._return = self._target(*self._args, **self._kwargs)
+
+    def join(self):
+        super().join()
+        return self._return
+
+    def value():
+        return self._return
 
 class TestNamedLockDecorator(TestCase):
     def setUp(self):
         pass
 
-    def test_tests_working(self):
-        async def run_tests():
-            task_run = asyncio.create_task(handle_named_lock())
+    def test_handle_named_lock_working(self):
+        to_pass = CustomThread(target=sleep_func)
+        to_fail = CustomThread(target=sleep_func)
 
-            task_bail = asyncio.create_task(handle_named_lock())
+        to_pass.start()
+        to_fail.start()
 
-            self.assertNotEqual(await task_bail, None)
-            self.assertNotEqual(await task_run, True)
+        to_fail.join()
+        to_pass.join()
 
-        asyncio.run(run_tests())
+        self.assertEqual(to_pass.value(), True)
+        self.assertEqual(to_fail.value(), None)
